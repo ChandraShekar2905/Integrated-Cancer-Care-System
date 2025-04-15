691f713df492b5380783c069be850af035fcc3a5
AaryakPawar <pawar.aar@northeastern.edu>
2025-04-15 18:27:24 -0400

    Implemented Role.java where every other role in Business.Role package is Returned.

--- AedFinalProject/src/Business/Role/Role.java
+++ AedFinalProject/src/Business/Role/Role.java
@@ -1,13 +1,71 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
  */
 package Business.Role;
-
 /**
  *
- * @author hrishipal
+ * @author aaryakpawar
  */
-public class Role {
+
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+
+public abstract class Role {
     
+    private String type; // Add a type field to store the subclass name
+
+    public Role() {
+        this.type = this.getClass().getSimpleName(); // Automatically set the type field to the subclass name
+    }
+
+    public enum RoleType {
+        Admin("Admin"),
+        RadiologistDoctor("RadiologistDoctor"),
+        OncologistDoctor("OncologistDocotr"),
+        LabAssistant("Lab Assistant"),
+        Patient("Patient"),
+        Pharmacist("Pharmacist"),
+        Accountant("Accountant"),
+        InsuranceAgent("Insurance Agent"),
+        InsurancePlanner("Insurance Policy Planner"),
+        InsuranceFinanceManager("Insurance Finance Manager"),
+        GovernmentHealthOfficer("Government Health Officer"),
+        GovernmentTreasurer("Government Treasure"),
+        GovernmentSecretaries("Government Secretaries");
+
+        private String value;
+
+        private RoleType(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return value;
+        }
+    }
+
+    public abstract JPanel createWorkArea(JPanel userProcessContainer,
+            UserAccount account,
+            Organization organization,
+            Enterprise enterprise,
+            EcoSystem ecosystem);
+    
+    public String getType() {
+        return type; // Getter for the type field
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName();
+    }
+
 }
