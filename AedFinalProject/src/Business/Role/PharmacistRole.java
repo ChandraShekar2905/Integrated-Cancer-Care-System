bb2f8d8dd7eb323b7104b3bf42873d07f73942dc
AaryakPawar <pawar.aar@northeastern.edu>
2025-04-15 19:36:30 -0400

    Added PharmacistRole and connected it to PharmacistRequestAssignmentJPanel.

---
+++ AedFinalProject/src/Business/Role/PharmacistRole.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Role;
+
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Organization.DoctorOrganization;
+import Business.Organization.Organization;
+import Business.Organization.PharmacyOrganization;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+import userinterface.pharmacist.PharmacistRequestAssignmentJPanel;
+
+/**
+ *
+ * @author aaryakpawar
+ */
+public class PharmacistRole extends Role {
+    
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount userAccount, Organization organization, Enterprise enterprise, EcoSystem business) {
+        return new PharmacistRequestAssignmentJPanel(userProcessContainer, userAccount, (PharmacyOrganization)organization, enterprise);
+    }
+}
