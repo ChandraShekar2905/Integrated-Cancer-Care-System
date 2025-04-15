f02e9e6ea8bf9154ca0735a35bc89ddd1c9ad076
ChandraShekar2905 <kusukunthla.c@northeastern.edu>
2025-04-15 17:27:25 -0400

    Created the UI of Login Page

--- AedFinalProject/src/Business/EcoSystem.java
+++ AedFinalProject/src/Business/EcoSystem.java
@@ -17,4 +17,51 @@ import java.util.ArrayList;
 // Extending Organization from ECosystem
 public class EcoSystem extends Organization{
     
+       
+    private static EcoSystem ecosystem;
+    private ArrayList<Network> networks;
+    public static EcoSystem getInstance(){
+        if(ecosystem==null){
+            ecosystem=new EcoSystem();
+        }
+        return ecosystem;
+    }
+    
+    public Network createAndAddNetwork(){
+        Network network=new Network();
+        networks.add(network);
+        return network;
+    }
+    //Overriding Support role
+    //Created ArrayList of Role
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roleList=new ArrayList<Role>();
+        roleList.add(new SystemAdminRole());
+        return roleList;
+    }
+    private EcoSystem(){
+        super(null);
+        networks=new ArrayList<Network>();
+    }
+    //ArrayList of <Network> calling function getNetwork() 
+    public ArrayList<Network> getNetworks() {
+        return networks;
+    }
+    //ArrayList of <Network> calling function setNetwork()
+    public void setNetworks(ArrayList<Network> networks) {
+        this.networks = networks;
+    }
+    
+    //Defining boolean function for Unique User
+    public boolean IsUserUnique(String userName){
+        if(!this.getUserAccountDirectory().isUserUnique(userName)){
+            return false;
+        }
+        for(Network network:networks){
+            
+        }
+        return true;
+    }
+    
 }
