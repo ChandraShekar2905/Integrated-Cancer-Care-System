9224985121f8ea7ed391516c6104f268ff3acb71
HrishiPal21 <pal.h@northeastern.edu>
2025-04-19 19:17:19 -0400

    minor changes

--- AedFinalProject/src/Business/Organization/SecretaryOrganization.java
+++ AedFinalProject/src/Business/Organization/SecretaryOrganization.java
@@ -5,7 +5,7 @@
  */
 package Business.Organization;
 
-import Business.Role.AdministrationSecretaries;
+import Business.Role.AdministrationSecretary;
 import Business.Role.Role;
 import java.util.ArrayList;
 
@@ -22,7 +22,7 @@ public class SecretaryOrganization extends Organization {
     @Override
     public ArrayList<Role> getSupportedRole() {
         ArrayList<Role> roles = new ArrayList();
-        roles.add(new AdministrationSecretaries());
+        roles.add(new AdministrationSecretary());
         return roles;
     }
     
